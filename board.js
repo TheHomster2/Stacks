e64806bc04b0b35882fc06b3b4e78af25f65e091
JoeSuzuki <joesuzuki516@gmail.com>
2018-08-02 19:36:31 -0400

    Match the progress bar height with the small buttons

--- board.js
+++ board.js
@@ -9,5 +9,5 @@ var buttonHTML = "<div class=\"addCardButton\"><button class=\"btn btn-sm btn-ou
 var cardHTML = "<div class=\"card bg-light mb-3\" id=\"cards-select\" style=\"max-width: 18rem;\"><div class=\"card-header\">Header</div><div class=\"card-body\"><h5 class=\"card-title\">Basic card </h5><p class=\"card-text\">Some quick example text to build on the card title and make up the bulk of the card's content.</p></div></div>";
 
 function addCard() {
-  document.querySelector(".board").querySelector(".addCardButton").innerHTML = cardHTML + buttonHTML; 
+  document.querySelector(".board").querySelector(".addCardButton").innerHTML = cardHTML; 
 }
