b9e892b9b15305280a544e0fbe768e7abf8fff21
JoeSuzuki <joesuzuki516@gmail.com>
2018-08-02 15:43:13 -0400

    Fix UI

--- board.js
+++ board.js
@@ -1,1 +1,11 @@
-document.getElementsByClassName("Board").getElementsByClassName("cards-select")
+// document.getElementsByClassName("Board").getElementsByClassName("cards-select")
+
+// board class
+// cards-select id
+// addCardButton button
+// new-cards div
+// var addCardButton =
+
+function addCard() {
+  document.getElementsByClassName("board").getElementById("new-cards").innerHTML = "<h1>Hello</h1>"
+}
