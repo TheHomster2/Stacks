decbf0ffbe506f33cd270e00cb7c2632d35c56b8
JoeSuzuki <joesuzuki516@gmail.com>
2018-08-02 16:57:23 -0400

    Add edit button for cards

--- board.js
+++ board.js
@@ -6,7 +6,8 @@
 // new-cards div
 // var addCardButton =
 var buttonHTML = "<div class=\"addCardButton\"><button class=\"btn btn-sm btn-outline-secondary\" style=\"width: 100%;\" onClick=\"addCard()\"><span data-feather=\"plus\"></span> Add another card</button></div>";
+var cardHTML = "<div class=\"card bg-light mb-3\" id=\"cards-select\" style=\"max-width: 18rem;\"><div class=\"card-header\">Header</div><div class=\"card-body\"><h5 class=\"card-title\">Basic card </h5><p class=\"card-text\">Some quick example text to build on the card title and make up the bulk of the card's content.</p></div></div>";
 
 function addCard() {
-  document.querySelector(".board").querySelector(".addCardButton").innerHTML = "<h1>Hello</h1>" + buttonHTML 
+  document.querySelector(".board").querySelector(".addCardButton").innerHTML = cardHTML + buttonHTML; 
 }
