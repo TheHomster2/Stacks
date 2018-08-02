e63bd58b4fd93fd94a47066f087a8920ae695b79
JoeSuzuki <joesuzuki516@gmail.com>
2018-08-02 16:35:39 -0400

    Button shows new html

--- board.js
+++ board.js
@@ -5,7 +5,8 @@
 // addCardButton button
 // new-cards div
 // var addCardButton =
+var buttonHTML = "<div class=\"addCardButton\"><button class=\"btn btn-sm btn-outline-secondary\" style=\"width: 100%;\" onClick=\"addCard()\"><span data-feather=\"plus\"></span> Add another card</button></div>";
 
 function addCard() {
-  document.querySelector(".board").getElementsByClassName(".addCardButton")[0].innerHTML = "<h1>Hello</h1>"
+  document.querySelector(".board").querySelector(".addCardButton").innerHTML = "<h1>Hello</h1>" + buttonHTML 
 }
