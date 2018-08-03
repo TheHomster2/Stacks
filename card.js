1b809b46969178b9b846052acd0359f1ea71404d
JoeSuzuki <joesuzuki516@gmail.com>
2018-08-03 12:38:10 -0400

    Started react app

--- card.js
+++ card.js
@@ -1,5 +1,5 @@
 var buttonHTML = "<div class=\"addCardButton\"><button class=\"btn btn-sm btn-outline-secondary\" style=\"width: 100%;\" onClick=\"addCard()\"><span data-feather=\"plus\"></span> Add another card</button></div>";
-var cardHTML = "<div class=\"card bg-light mb-3\" id=\"cards-select\" style=\"max-width: 18rem;\"><div class=\"card-header\">Header</div><div class=\"card-body\"><h5 class=\"card-title\">Basic card </h5><p class=\"card-text\">Some quick example text to build on the card title and make up the bulk of the card's content.</p></div></div>";
+var cardHTML = "<div class=\"card bg-light mb-3 cards-select\" style=\"max-width: 18rem;\"><div class=\"card-header d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2\">Finish popup<div><span data-feather=\"alert-circle\"></span>Aug 10</div></div><div class=\"card-body\"><h5 class=\"card-title\">User</h5><p class=\"card-text\">Some quick example text to build on the card title and make up the bulk of the card's content.</p></div></div>";
 
 function createCard() {
 
@@ -15,7 +15,25 @@ function deleteCard() {
 }
 
 function addCard() {
-  document.querySelector(".board").querySelector(".addCardButton").innerHTML = cardHTML; 
+  // document.querySelector(".board").querySelector(".addCardButton").innerHTML = cardHTML; 
+  var boards = document.getElementsByClassName('board');
+  var cards = document.getElementsByClassName('cards-select');
+
+  for (var i = 0; i < cards.length; i++){
+    console.log(cards[i].textContent);
+  }
+  console.log(cards.length)
+  console.log(boards.length)
+
+  var newEl = document.createElement('li');
+  var newText = document.createTextNode('node');
+  
+  newEl.appendChild(newText);
+  var position = document.querySelectorAll('div.cards-select');
+  var nextItem = document.querySelectorAll('div.cards-select')[0].parentNode;
+
+  nextItem.appendChild(newEl);
+  // position[0].appendChild(newEl);
 }
 {/* <script>
   $(document).ready(function() {
