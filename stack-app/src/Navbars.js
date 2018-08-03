77ed585af4bb7df8fa5e58255d2cb21e97c40b36
JoeSuzuki <joesuzuki516@gmail.com>
2018-08-03 15:21:39 -0400

    Finished with sidebar but loads in wrong area

--- stack-app/src/Navbars.js
+++ stack-app/src/Navbars.js
@@ -12,13 +12,13 @@ import {
   DropdownMenu,
   DropdownItem } from 'reactstrap';
 
-export default class Example extends React.Component {
+export default class Navbars extends React.Component {
   render() {
     return (
-      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Stack</a>
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
+      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow ">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Stack</a>
+          <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
           <a class="nav-link" href="#">Sign out</a>
         </li>
       </ul>
--- stack-app/src/Sidebar.js
+++ stack-app/src/Sidebar.js
@@ -4,9 +4,10 @@ import {
   NavLink,
   HashRouter
 } from "react-router-dom";
-import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
+import { Home } from 'react-feather';
+import { Nav, NavItem} from 'reactstrap';
 import './Dashboard.css';
 import './Sidebar.css';
 
@@ -14,37 +15,40 @@ class Sidebar extends Component {
   render() {
     return (
       <div class="container-fluid">
-        <div class="row">
-          <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-            <div class="sidebar-sticky">
-              <ul class="nav flex-column">
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    <span data-feather="home"></span>
-                    Dashboard <span class="sr-only">(current)</span>
-                  </a>
-                </li>
-              </ul>
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="home"></span>
+                  Dashboard <span class="sr-only">(current)</span>
+                </a>
+              </li>
+            </ul>
+            <HashRouter>
+          <div>
+          <Nav vertical>
+            <NavItem>
+              <NavLink exact to="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/stuff">Stuff</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/contact">Contact</NavLink>
+            </NavItem>
+            <div className="content">
+              <Route path="/stuff" component={Stuff}/>
+              <Route path="/contact" component={Contact}/>
             </div>
-          </nav>
-        </div>           
-      </div>
-
-        // <HashRouter>
-        //   <div>
-        //     <h1>Simple SPA</h1>
-        //     <ul className="header">
-        //       <li><NavLink exact to="/">Home</NavLink></li>
-        //       <li><NavLink to="/stuff">Stuff</NavLink></li>
-        //       <li><NavLink to="/contact">Contact</NavLink></li>
-        //     </ul>
-        //     <div className="content">
-        //       <Route exact path="/" component={Home}/>
-        //       <Route path="/stuff" component={Stuff}/>
-        //       <Route path="/contact" component={Contact}/>
-        //     </div>
-        //   </div>
-        // </HashRouter> 
+            </Nav>
+          </div>
+        </HashRouter> 
+          </div>
+        </nav>
+      </div>          
+    </div>
       );
     }
   } 
